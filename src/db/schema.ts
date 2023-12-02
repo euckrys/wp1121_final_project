d224b84a0b0ca4d27b8f41fad22c9ef5153018f1
WeiLung92 <weilung92@gmail.com>
2023-12-03 00:40:32 +0800

    problem: sign in add isCoach

---
+++ src/db/schema.ts
@@ -0,0 +1,23 @@
+import { index, pgTable, serial, uuid, varchar, boolean } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey(),
+    displayId: uuid("display_id").defaultRandom().notNull().unique(),
+    isCoach: boolean("isCoach").notNull(),
+    username: varchar("username", { length: 100 }).notNull(),
+    email: varchar("email", { length: 100 }).notNull().unique(),
+    hashedPassword: varchar("hashed_password", { length: 100 }),
+    provider: varchar("provider", {
+      length: 100,
+      enum: ["github", "credentials"],
+    })
+      .notNull()
+      .default("credentials"),
+  },
+  (table) => ({
+    displayIdIndex: index("display_id_index").on(table.displayId),
+    emailIndex: index("email_index").on(table.email),
+  }),
+);
---
+++ src/lib/types/db.ts
@@ -0,0 +1,7 @@
+export type User = {
+  id: string;
+  isCoach: boolean;
+  username: string;
+  email: string;
+  provider: "github" | "credentials";
+};
